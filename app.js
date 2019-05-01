f696e3d4c5b5d8dae41ceb68483e6a9dc921e17f
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-01 19:29:52 +0530

    Event model in-memory.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const {buildSchema} = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
@@ -12,12 +14,27 @@ app.use(
     graphqlHttp({
       graphiql: true,
       schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
         type RootQuery {
-          events: [String!]!
+          events: [Event!]!
         }
 
         type RootMutation {
-          createEvent(name: String): String
+          createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -27,12 +44,22 @@ app.use(
       `),
       rootValue: {
         events: () => {
-          return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+          return events;
         },
         createEvent: (args) => {
-          const eventName = args.name;
+          const {title, description, price, date} = args.eventInput;
 
-          return eventName;
+          const event = {
+            _id: Math.random().toString(),
+            title,
+            description,
+            price,
+            date,
+          };
+
+          events.push(event);
+
+          return event;
         },
       },
     })

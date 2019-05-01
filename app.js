5a25596149f6284a26996de13f5839b40b227e34
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-01 17:41:37 +0530

    Basic graphql implementation.

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  return res.send('Hello World!');
-});
+app.use(
+    '/graphql',
+    graphqlHttp({
+      graphiql: true,
+      schema: buildSchema(`
+        type RootQuery {
+          events: [String!]!
+        }
+
+        type RootMutation {
+          createEvent(name: String): String
+        }
+
+        schema {
+          query: RootQuery
+          mutation: RootMutation
+        }
+      `),
+      rootValue: {
+        events: () => {
+          return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        },
+        createEvent: (args) => {
+          const eventName = args.name;
+
+          return eventName;
+        },
+      },
+    })
+);
 
 app.listen(3000);

2d05edf7de41d298e2b4a6cb73292b9a8256bb5e
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-04 18:47:15 +0530

    Allow cross origin requests.

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headres', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  return next();
+});
+
 app.use(isAuth);
 
 app.use(
